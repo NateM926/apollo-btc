a028e51dbd881c7ecbf88f1d333678000260531b
NateM926 <natem926rr@gmail.com>
2020-10-19 12:15:57 -0600

    refactor ExchangeRates

---
+++ src/ExchangeRates.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { NetworkStatus, useQuery, gql } from "@apollo/client";
+
+export default function ExchangeRates() {
+  const { loading, error, data, networkStatus, refetch } = useQuery(
+    gql`
+      {
+        rates(currency: "USD") {
+          currency
+          rate
+        }
+      }
+    `,
+    {
+      fetchPolicy: "no-cache", // testing cache policies in the integration
+      notifyOnNetworkStatusChange: true
+    }
+  );
+
+  if (networkStatus === NetworkStatus.refetch) return "Refetching!";
+  if (loading) return <p>Loading...</p>;
+
+  const submit = (e) => {
+    e.preventDefault();
+    const requestOptions = {
+      method: "GET",
+      headers: {
+        // "Content-Type": "application/json" // this header breaks the integration
+        "Content-Type": "application/x-www-form-urlencoded" // this header works in the integration
+      }
+    };
+
+    fetch(
+      "https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((commits) => alert(JSON.stringify(commits)));
+  };
+
+  if (error)
+    return (
+      <div>
+        <p>{`Error :( ${error.message}`}</p>
+        <button onClick={() => refetch()}>Refetch!</button>
+        <form onSubmit={(e) => submit(e)}>
+          <button type="submit">TEST SUBMIT FETCH</button>
+        </form>
+      </div>
+    );
+
+  return data.rates.map(({ currency, rate }) => (
+    <div key={currency}>
+      <p>
+        {currency}: {rate}
+      </p>
+    </div>
+  ));
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import React from "react";
 import { render } from "react-dom";
-import {
-  ApolloClient,
-  NetworkStatus,
-  InMemoryCache,
-  ApolloProvider,
-  useQuery,
-  gql
-} from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import useUnload from "./useUnload";
+import ExchangeRates from "./ExchangeRates";
 
 function App() {
   const client = new ApolloClient({
@@ -16,78 +10,10 @@ function App() {
     cache: new InMemoryCache()
   });
 
-  // testing close event in the integration (beforeunload / onclose)
-  // window.addEventListener("beforeunload", function (e) {
-  //   // Cancel the event as stated by the standard.
-  //   e.preventDefault();
-
-  //   var confirmationMessage = "o/";
-
-  //   (e || window.event).returnValue = confirmationMessage; //Gecko + IE
-  //   return confirmationMessage; //Webkit, Safari, Chrome
-  // });
-
-  function ExchangeRates() {
-    useUnload((e) => {
-      e.preventDefault();
-      e.returnValue = "";
-    });
-
-    const { loading, error, data, networkStatus, refetch } = useQuery(
-      gql`
-        {
-          rates(currency: "USD") {
-            currency
-            rate
-          }
-        }
-      `,
-      {
-        fetchPolicy: "no-cache", // testing cache policies in the integration
-        notifyOnNetworkStatusChange: true
-      }
-    );
-
-    if (networkStatus === NetworkStatus.refetch) return "Refetching!";
-    if (loading) return <p>Loading...</p>;
-
-    const submit = (e) => {
-      e.preventDefault();
-      const requestOptions = {
-        method: "GET",
-        headers: {
-          // "Content-Type": "application/json" // this header breaks the integration
-          "Content-Type": "application/x-www-form-urlencoded" // this header works in the integration
-        }
-      };
-
-      fetch(
-        "https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits",
-        requestOptions
-      )
-        .then((response) => response.json())
-        .then((commits) => alert(JSON.stringify(commits)));
-    };
-
-    if (error)
-      return (
-        <div>
-          <p>{`Error :( ${error.message}`}</p>
-          <button onClick={() => refetch()}>Refetch!</button>
-          <form onSubmit={(e) => submit(e)}>
-            <button type="submit">TEST SUBMIT FETCH</button>
-          </form>
-        </div>
-      );
-
-    return data.rates.map(({ currency, rate }) => (
-      <div key={currency}>
-        <p>
-          {currency}: {rate}
-        </p>
-      </div>
-    ));
-  }
+  useUnload((e) => {
+    e.preventDefault();
+    e.returnValue = "";
+  });
 
   return (
     <ApolloProvider client={client}>
