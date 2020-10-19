e5c25a34bbc98f74f3741f8cbbfb6b67a7caa327
NateM926 <natem926rr@gmail.com>
2020-10-19 12:38:31 -0600

    bring back exchange rates

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import { render } from "react-dom";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import useUnload from "./useUnload";
-// import ExchangeRates from "./ExchangeRates";
+import ExchangeRates from "./ExchangeRates";
 
 function App() {
-  // useUnload((e) => {
-  //   e.preventDefault();
-  //   e.returnValue = "";
-  // });
+  useUnload((e) => {
+    e.preventDefault();
+    e.returnValue = "";
+  });
 
   const client = new ApolloClient({
     uri: "https://48p1r2roz4.sse.codesandbox.io",
@@ -19,11 +19,7 @@ function App() {
     <ApolloProvider client={client}>
       <div>
         <h2>Coinbase GQL Example</h2>
-        {useUnload((e) => {
-          e.preventDefault();
-          e.returnValue = "";
-        })}
-        {/* <ExchangeRates /> */}
+        <ExchangeRates />
       </div>
     </ApolloProvider>
   );
